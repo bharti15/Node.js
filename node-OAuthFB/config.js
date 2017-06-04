6a445dc930f3cf77bf5c1dedb7eca7f9743ad112
bharti15 <y14uc068@lnmiit.ac.in>
2017-06-05 01:56:38 +0530

    Add files via upload

---
+++ node-OAuthFB/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+	'secretKey': '12345-67890-09876-54321',
+	'mongoUrl': 'mongodb://localhost:27017/conFusion',
+	 'facebook': {
+        clientID: '1792193167762866',
+        clientSecret: '97c7ce5a741930b7e817cc3ba165f08e',
+        callbackURL: 'https://localhost:3443/users/facebook/callback'
+    }
+}
